AOSPA/android_device_qcom_common

dlkm: Add kalama in-list
Change-Id: I4104f7a9e1fd0f4d0818b5f4c5552c7399acce26

dlkm/AndroidKernelModule.mk
@@ -1,5 +1,5 @@
 ifneq ($(KERNEL_MODULES_INSTALL),)
-ifneq ($(call is-board-platform-in-list, bengal taro crow), true)
+ifneq ($(call is-board-platform-in-list, kalama bengal taro crow), true)
 
 # Get the number of CPU cores. This is the number of parallel jobs to be passed to make command.
 NCORES := $(shell grep -c ^processor /proc/cpuinfo)

janakniraula/device_qcom_common

dlkm/AndroidKernelModule.mk
@@ -1,5 +1,5 @@
 ifneq ($(KERNEL_MODULES_INSTALL),)
-ifneq ($(call is-board-platform-in-list, bengal taro crow), true)
+ifneq ($(call is-board-platform-in-list, bengal kalama taro crow), true)
 
 # Get the number of CPU cores. This is the number of parallel jobs to be passed to make command.
 NCORES := $(shell grep -c ^processor /proc/cpuinfo)